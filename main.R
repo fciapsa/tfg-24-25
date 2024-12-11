b664988428dc53ff30b8470d28bc24a371f812fe
SrTine <81743854+SrTine@users.noreply.github.com>
2024-12-11 19:41:22 +0100

    Add files via upload

--- main.R
+++ main.R
@@ -1,136 +1,167 @@
 #######################################################################################################
+#############TFG 2024/2025 - Ciclo del Agua - Flavius Abel Ciapsa & Pablo Martínez Domingo#############
+#######################################################################################################
+
+#INI#____________CONFIGURAR DIRECTORIO ACTUAL____________#INI#
+current_directory = dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(current_directory)
+#FIN#____________CONFIGURAR DIRECTORIO ACTUAL____________#FIN#
+
 #INI#____________CARGA ARCHIVOS R AUXILIARES____________#INI#
 source( "funciones_comunes.R" )
 #FIN#____________CARGA ARCHIVOS R AUXILIARES____________#FIN#
 
 #INI#____________CONFIGURACIÓN INICIAL DEPENDENCIAS____________#INI#
-## Instalación de paquetes necesarios (solo necesario la primera vez)
+## Instalación de paquetes necesarios (solo necesario la primera vez que se ejecuta)
 #carga_paquetes()
 ## Carga de librerías necesarias
 librerias()
 #FIN#____________CONFIGURACIÓN INICIAL DEPENDENCIAS____________#FIN#
 
-# Configurar el directorio que contiene este script como el directorio de trabajo
-current_directory = dirname(rstudioapi::getActiveDocumentContext()$path)
-setwd(current_directory)
+#INI#____________LECTURA DE LOS CSVS____________#INI#
 
-# Carga de .csv necesarios
+#Captación realizada por la propia empresa por comunidades y ciudades autónomas, tipo de captación y periodo
+# Fuente: https://www.ine.es/up/96pyDkGB
+df_captac <- read_delim("csvs/Captación realizada por la propia empresa por comunidades y ciudades autónomas, tipo de captación y periodo.csv", 
+                        delim = ";",
+                        col_types = cols(periodo = col_integer(), Total = col_number()),
+                        locale = locale(decimal_mark = ",", grouping_mark = "."),
+                        na = "..")
 
 # Distribución de agua registrada, usuario y periodo.
 # Fuente: https://ine.es/up/izJimhrq
 aux1 <- read_delim("csvs/Distribución de agua registrada, usuario y periodo.csv",
-            delim = ";",
-            col_types = cols(periodo = col_integer(), Total = col_number()),
-            locale = locale(decimal_mark = ",", grouping_mark = "."),
-            na = "..")
+                   delim = ";",
+                   col_types = cols(periodo = col_integer(), Total = col_number()),
+                   locale = locale(decimal_mark = ",", grouping_mark = "."),
+                   na = "..")
 
 # Volumen de agua disponible (potabilizada y no potabilizada) por comunidades y
 # ciudades autónomas, tipo de indicador y periodo.
 # Fuente: https://ine.es/up/sHkVfTve
 aux2 <- read_delim("csvs/Volumen de agua disponible (potabilizada y no potabilizada) por comunidades y ciudades autónomas, tipo de indicador y periodo.csv",
-            delim = ";",
-            col_types = cols(periodo = col_integer(), Total = col_number()),
-            locale = locale(decimal_mark = ",", grouping_mark = "."))
+                   delim = ";",
+                   col_types = cols(periodo = col_integer(), Total = col_number()),
+                   locale = locale(decimal_mark = ",", grouping_mark = "."))
 
 # Distribución de agua registrada por comunidades y ciudades autónomas, grupos de usuarios e importe y periodo.
 # Fuente: https://ine.es/up/WMtOBGH0
 aux3 <- read_delim("csvs/Distribución de agua registrada por comunidades y ciudades autónomas, grupos de usuarios e importe y periodo.csv", 
-            delim = ";",
-            col_types = cols(periodo = col_integer(), Total = col_number()),
-            locale = locale(decimal_mark = ",", grouping_mark = "."),
-            na = "..")
+                   delim = ";",
+                   col_types = cols(periodo = col_integer(), Total = col_number()),
+                   locale = locale(decimal_mark = ",", grouping_mark = "."),
+                   na = "..")
 
 # Volumen de agua suministrada a la red por comunidades y ciudades autónomas, tipo de indicador y periodo.
 # Fuente: https://ine.es/up/ZQ5YmzAI
 aux4 <- read_delim("csvs/Volumen de agua suministrada a la red por comunidades y ciudades autónomas, tipo de indicador y periodo.csv", 
-            delim = ";",
-            col_types = cols(periodo = col_integer(), Total = col_number()),
-            locale = locale(decimal_mark = ",", grouping_mark = "."),
-            na = "..")
+                   delim = ";",
+                   col_types = cols(periodo = col_integer(), Total = col_number()),
+                   locale = locale(decimal_mark = ",", grouping_mark = "."),
+                   na = "..")
 
 # Recogida y tratamiento de las aguas residuales por comunidades y ciudades autónomas, tipo de indicador y periodo.
 # Fuente: https://ine.es/up/2Hs7okgKi1
 aux5 <- read_delim("csvs/Recogida y tratamiento de las aguas residuales por comunidades y ciudades autónomas, tipo de indicador y periodo.csv", 
-            delim = ";",
-            col_types = cols(periodo = col_integer(), Total = col_number()),
-            locale = locale(decimal_mark = ",", grouping_mark = "."),
-            na = "..")
+                   delim = ";",
+                   col_types = cols(periodo = col_integer(), Total = col_number()),
+                   locale = locale(decimal_mark = ",", grouping_mark = "."),
+                   na = "..")
+#FIN#____________LECTURA DE LOS CSVS____________#FIN#
 
-# Renombramiento de las columnas utilizando estilo snake_case
-names(aux1)[1] <- "grupos_usuarios"
-# names(aux1)[2] <- "periodo"
-names(aux1)[3] <- "total"
+#INI#____________CONFIGURADACION DE LOS DF OBTENIDOS____________#INI#
 
-names(aux2)[1] <- "total_nacional"
-names(aux2)[2] <- "comunidades_ciudades_autonomas"
-names(aux2)[3] <- "tipo_indicador"
-# names(aux2)[4] <- "periodo"
-names(aux2)[5] <- "total"
+########################################################################################################
+#Asociamos el valor "Ceuta y Melilla" a la columna 2 del df "df_captac",
+#que se encuentra vacia para estos casos en el csv inicial
+num_filas <- 1
 
-names(aux3)[1] <- "total_nacional"
-names(aux3)[2] <- "comunidades_ciudades_autonomas"
-names(aux3)[3] <- "grupos_usuarios_importe"
-# names(aux3)[4] <- "periodo"
-names(aux3)[5] <- "total"
+while (num_filas <= nrow(df_captac)) {
+  if (df_captac[num_filas,1] == "Ceuta y Melilla") {
+    df_captac[num_filas,2] <- "Ceuta y Melilla"
+  } 
+  num_filas <- num_filas + 1
+}
 
-names(aux4)[1] <- "total_nacional"
-names(aux4)[2] <- "comunidades_ciudades_autonomas"
-names(aux4)[3] <- "tipo_indicador"
-# names(aux4)[4] <- "periodo"
-names(aux4)[5] <- "total"
+#Filtramos por este valor en el df para comprobar la actualización
+filter(df_captac, `Total Nacional` == "Ceuta y Melilla") %>%
+  mutate(`Comunidades y Ciudades Autónomas 2` <- "Ceuta y Melilla")
+########################################################################################################
 
-names(aux5)[1] <- "total_nacional"
-names(aux5)[2] <- "comunidades_ciudades_autonomas"
-names(aux5)[3] <- "tipo_indicador"
-# names(aux5)[4] <- "periodo"
-names(aux5)[5] <- "total"
+########################################################################################################
+#Renombrar la columna 1 del df "aux1"
+names(aux1)[1]  <- 'Usuarios'
 
-# Limpieza: Eliminación de las filas sin valores
-aux1 <- drop_na(aux1, "total")
-aux2 <- drop_na(aux2, "total")
-aux3 <- drop_na(aux3, "total")
-aux4 <- drop_na(aux4, "total")
-aux5 <- drop_na(aux5, "total")
+#Mostrar valores únicos del df
+unique((aux1$Total))
 
-# Combinación de las columnas "total_nacional" y "comunidades_ciudades_autonomas"
-# en una única columna que sintetiza la información de ambas
-aux2[aux2$total_nacional == "Total Nacional" &
-       is.na(aux2$comunidades_ciudades_autonomas),]$comunidades_ciudades_autonomas = "00 España"
-aux2[aux2$total_nacional == "Ceuta y Melilla" &
-       is.na(aux2$comunidades_ciudades_autonomas),]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
+#Borrar valores NA del df
+aux1 <- na.omit(aux1)
 
-
-aux3[aux3$total_nacional == "Total Nacional" &
-       aux3$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "00 España"
-aux3[aux3$total_nacional == "Ceuta y Melilla" &
-       aux3$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
-
-aux4[aux4$total_nacional == "Total Nacional" &
-       aux4$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "00 España"
-aux4[aux4$total_nacional == "Ceuta y Melilla" &
-       aux4$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
-
-aux5[aux5$total_nacional == "Total Nacional" &
-       aux5$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "00 España"
-aux5[aux5$total_nacional == "Ceuta y Melilla" &
-       aux5$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
-
-# Factorización de las columnas que contienen datos categóricos
-aux1$grupos_usuarios <- factor(aux1$grupos_usuarios)
+#Factorizamos las columnas
+aux1$Usuarios <- factor(aux1$Usuarios)
 aux1$periodo <- factor(aux1$periodo)
+########################################################################################################
 
-aux2$comunidades_ciudades_autonomas <- factor(aux2$comunidades_ciudades_autonomas)
-aux2$tipo_indicador <- factor(aux2$tipo_indicador)
-aux2$periodo <- factor(aux2$periodo)
 
-aux3$comunidades_ciudades_autonomas <- factor(aux3$comunidades_ciudades_autonomas)
-aux3$grupos_usuarios_importe <- factor(aux3$grupos_usuarios_importe)
-aux3$periodo <- factor(aux3$periodo)
+#FIN#____________CONFIGURADACION DE LOS DF OBTENIDOS____________#FIN#
 
-aux4$comunidades_ciudades_autonomas <- factor(aux4$comunidades_ciudades_autonomas)
-aux4$tipo_indicador <- factor(aux4$tipo_indicador)
-aux4$periodo <- factor(aux4$periodo)
 
-aux5$comunidades_ciudades_autonomas <- factor(aux5$comunidades_ciudades_autonomas)
-aux5$tipo_indicador <- factor(aux5$tipo_indicador)
-aux5$periodo <- factor(aux5$periodo)
+#INI#____________VISUALIZACIÓN DE LOS DFs____________#INI#
+
+#Diagrama de barras de df "aux1" categorizado por tipos de usuarios
+ggplot(aux1) +
+  geom_col(aes(x = periodo, y = Total, fill = Usuarios), position = "dodge") +
+  labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
+        # arriba(top), izquierda (left), derecha (right), abajo (bottom)
+        legend.position = "right"  
+  ) 
+
+
+aux1_sin_totales <- filter(aux1, Usuarios != 'Total')
+table(aux1$Usuarios, aux1$Total)
+
+#Diagrama de barras de df "aux1_sin_totales" categorizado por tipos de usuarios
+ggplot(aux1_sin_totales) +
+  geom_col(aes(x = periodo, y = Total, fill = Usuarios), position = "dodge") +
+  labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
+        # arriba(top), izquierda (left), derecha (right), abajo (bottom)
+        legend.position = "bottom"  
+  ) 
+
+#Diagrama de barras de df "aux1_sin_totales" categorizado por periodo (año)
+ggplot(aux1_sin_totales) +
+  geom_col(aes(x = Usuarios, y = Total, fill = periodo ), position = "dodge") +
+  labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
+        # arriba(top), izquierda (left), derecha (right), abajo (bottom)
+        legend.position = "bottom"  
+  ) 
+
+#Diagrama de dispersión de df "aux1_sin_totales" categorizado por tipos de usuarios
+ggplot(aux1_sin_totales) +
+  geom_point(aes(x = periodo, y = Total, color = Usuarios)) +
+  labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8)) 
+
+#Diagrama de dispersión de df "aux1_sin_totales" categorizado por tipos de usuarios de manera individual en vertical
+ggplot(aux1_sin_totales) +
+  geom_point(aes(x = periodo, y = Total, color = Usuarios)) +
+  facet_grid( Usuarios ~ .) +
+  labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
+        legend.position = "bottom"  ) 
+
+#Diagrama de dispersión de df "aux1_sin_totales" categorizado por tipos de usuarios de manera individual en diferentes diagramas
+ggplot(aux1_sin_totales) +
+  geom_point(aes(x = periodo, y = Total, color = Usuarios)) +
+  facet_wrap( Usuarios ~ ., nrow = 2) +
+  labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
+        legend.position = "bottom"  )
+#FIN#____________VISUALIZACIÓN DE LOS DFs____________#FIN#
+
+#######################################################################################################
+#######################################################################################################
