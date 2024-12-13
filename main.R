1bb2a8be3044f4b6b1784aeb8c039d9e1250baef
Flavius <flaviusciapsa@gmail.com>
2024-12-13 18:07:11 +0100

    Volver a subir cambios de limpieza y renombramiento

--- main.R
+++ main.R
@@ -75,56 +75,110 @@ df_residuales <- read_delim("csvs/Recogida y tratamiento de las aguas residuales
                    na = "..")
 #FIN#____________LECTURA DE LOS CSVS____________#FIN#
 
-#INI#____________CONFIGURADACION DE LOS DF OBTENIDOS____________#INI#
+#INI#____________CONFIGURACION DE LOS DF OBTENIDOS____________#INI#
 
-########################################################################################################
-names(df_captacion)[2]  <- 'Comunidades y Ciudades Autonomas'
-names(df_captacion)[3]  <- 'Tipo de captacion'
+# Renombramiento de las columnas utilizando estilo snake_case
+names(df_captacion)[1]  <- 'total_nacional'
+names(df_captacion)[2]  <- 'comunidades_ciudades_autonomas'
+names(df_captacion)[3]  <- 'tipo_captacion'
+#names(df_captacion)[4]  <- 'periodo'
+names(df_captacion)[5]  <- 'total'
 
-#Asociamos el valor "Ceuta y Melilla" a la columna 2 del df "df_captacion",
-#que se encuentra vacia para estos casos en el csv inicial
-num_filas <- 1
+names(df_distribucion)[1] <- "grupos_usuarios"
+# names(df_distribucion)[2] <- "periodo"
+names(df_distribucion)[3] <- "total"
 
-while (num_filas <= nrow(df_captacion)) {
-  if (df_captacion[num_filas,1] == "Ceuta y Melilla") {
-    df_captacion[num_filas,2] <- " 18 Ceuta y Melilla"
-  }else if (df_captacion[num_filas,1] == "Total Nacional" & df_captacion[num_filas,2] == "") {
-    df_captacion[num_filas,2] <- "00 Total"
-  }
-  num_filas <- num_filas + 1
-}
+names(df_volumen)[1] <- "total_nacional"
+names(df_volumen)[2] <- "comunidades_ciudades_autonomas"
+names(df_volumen)[3] <- "tipo_indicador"
+# names(df_volumen)[4] <- "periodo"
+names(df_volumen)[5] <- "total"
 
-#Filtramos por este valor en el df para comprobar la actualización
-filter(df_captacion, `Total Nacional` == "Ceuta y Melilla") #%>%
-  #filter(df_captacion, `Comunidades y Ciudades Autonomas` == "18 Ceuta y Melilla") #%>%
-  #mutate(`Comunidades y Ciudades Autonomas` <- "18 Ceuta y Melilla")
+names(df_distribucion_categorizada)[1] <- "total_nacional"
+names(df_distribucion_categorizada)[2] <- "comunidades_ciudades_autonomas"
+names(df_distribucion_categorizada)[3] <- "grupos_usuarios_importe"
+# names(df_distribucion_categorizada)[4] <- "periodo"
+names(df_distribucion_categorizada)[5] <- "total"
 
-########################################################################################################
+names(df_volumen_categorizada)[1] <- "total_nacional"
+names(df_volumen_categorizada)[2] <- "comunidades_ciudades_autonomas"
+names(df_volumen_categorizada)[3] <- "tipo_indicador"
+# names(df_volumen_categorizada)[4] <- "periodo"
+names(df_volumen_categorizada)[5] <- "total"
 
-########################################################################################################
-#Renombrar la columna 1 del df "df_distribucion"
-names(df_distribucion)[1]  <- 'Usuarios'
+names(df_residuales)[1] <- "total_nacional"
+names(df_residuales)[2] <- "comunidades_ciudades_autonomas"
+names(df_residuales)[3] <- "tipo_indicador"
+# names(df_residuales)[4] <- "periodo"
+names(df_residuales)[5] <- "total"
 
-#Mostrar valores únicos del df
-unique((df_distribucion$Total))
+# Limpieza: Eliminación de las filas sin valores
+df_captacion <- drop_na(df_captacion, "total")
+df_distribucion <- drop_na(df_distribucion, "total")
+df_volumen <- drop_na(df_volumen, "total")
+df_distribucion_categorizada <- drop_na(df_distribucion_categorizada, "total")
+df_volumen_categorizada <- drop_na(df_volumen_categorizada, "total")
+df_residuales <- drop_na(df_residuales, "total")
 
-#Borrar valores NA del df
-df_distribucion <- na.omit(df_distribucion)
+# Combinación de las columnas "total_nacional" y "comunidades_ciudades_autonomas"
+# en una única columna que sintetiza la información de ambas
+df_captacion[df_captacion$total_nacional == "Total Nacional" &
+               df_captacion$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "00 España"
+df_captacion[df_captacion$total_nacional == "Ceuta y Melilla" &
+               df_captacion$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
 
-#Factorizamos las columnas
-df_distribucion$Usuarios <- factor(df_distribucion$Usuarios)
+df_volumen[df_volumen$total_nacional == "Total Nacional" &
+       is.na(df_volumen$comunidades_ciudades_autonomas),]$comunidades_ciudades_autonomas = "00 España"
+df_volumen[df_volumen$total_nacional == "Ceuta y Melilla" &
+       is.na(df_volumen$comunidades_ciudades_autonomas),]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
+
+df_distribucion_categorizada[df_distribucion_categorizada$total_nacional == "Total Nacional" &
+       df_distribucion_categorizada$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "00 España"
+df_distribucion_categorizada[df_distribucion_categorizada$total_nacional == "Ceuta y Melilla" &
+       df_distribucion_categorizada$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
+
+df_volumen_categorizada[df_volumen_categorizada$total_nacional == "Total Nacional" &
+       df_volumen_categorizada$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "00 España"
+df_volumen_categorizada[df_volumen_categorizada$total_nacional == "Ceuta y Melilla" &
+       df_volumen_categorizada$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
+
+df_residuales[df_residuales$total_nacional == "Total Nacional" &
+       df_residuales$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "00 España"
+df_residuales[df_residuales$total_nacional == "Ceuta y Melilla" &
+       df_residuales$comunidades_ciudades_autonomas == "",]$comunidades_ciudades_autonomas = "18 Ceuta y Melilla"
+
+# Factorización de las columnas que contienen datos categóricos
+df_captacion$comunidades_ciudades_autonomas <- factor(df_captacion$comunidades_ciudades_autonomas)
+df_captacion$tipo_captacion <- factor(df_captacion$tipo_captacion)
+df_captacion$periodo <- factor(df_captacion$periodo)
+
+df_distribucion$grupos_usuarios <- factor(df_distribucion$grupos_usuarios)
 df_distribucion$periodo <- factor(df_distribucion$periodo)
-########################################################################################################
 
+df_volumen$comunidades_ciudades_autonomas <- factor(df_volumen$comunidades_ciudades_autonomas)
+df_volumen$tipo_indicador <- factor(df_volumen$tipo_indicador)
+df_volumen$periodo <- factor(df_volumen$periodo)
 
-#FIN#____________CONFIGURADACION DE LOS DF OBTENIDOS____________#FIN#
+df_distribucion_categorizada$comunidades_ciudades_autonomas <- factor(df_distribucion_categorizada$comunidades_ciudades_autonomas)
+df_distribucion_categorizada$grupos_usuarios_importe <- factor(df_distribucion_categorizada$grupos_usuarios_importe)
+df_distribucion_categorizada$periodo <- factor(df_distribucion_categorizada$periodo)
+
+df_volumen_categorizada$comunidades_ciudades_autonomas <- factor(df_volumen_categorizada$comunidades_ciudades_autonomas)
+df_volumen_categorizada$tipo_indicador <- factor(df_volumen_categorizada$tipo_indicador)
+df_volumen_categorizada$periodo <- factor(df_volumen_categorizada$periodo)
+
+df_residuales$comunidades_ciudades_autonomas <- factor(df_residuales$comunidades_ciudades_autonomas)
+df_residuales$tipo_indicador <- factor(df_residuales$tipo_indicador)
+df_residuales$periodo <- factor(df_residuales$periodo)
+
+#FIN#____________CONFIGURACION DE LOS DF OBTENIDOS____________#FIN#
 
 
 #INI#____________VISUALIZACIÓN DE LOS DFs____________#INI#
 
 #Diagrama de barras de df "df_distribucion" categorizado por tipos de usuarios
 ggplot(df_distribucion) +
-  geom_col(aes(x = periodo, y = Total, fill = Usuarios), position = "dodge") +
+  geom_col(aes(x = periodo, y = total, fill = grupos_usuarios), position = "dodge") +
   labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
   theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
         # arriba(top), izquierda (left), derecha (right), abajo (bottom)
@@ -132,12 +186,11 @@ ggplot(df_distribucion) +
   ) 
 
 
-df_distribucion_sin_totales <- filter(df_distribucion, Usuarios != 'Total')
-table(df_distribucion$Usuarios, df_distribucion$Total)
+df_distribucion_sin_totales <- filter(df_distribucion, grupos_usuarios != 'Total')
 
 #Diagrama de barras de df "df_distribucion_sin_totales" categorizado por tipos de usuarios
 ggplot(df_distribucion_sin_totales) +
-  geom_col(aes(x = periodo, y = Total, fill = Usuarios), position = "dodge") +
+  geom_col(aes(x = periodo, y = total, fill = grupos_usuarios), position = "dodge") +
   labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
   theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
         # arriba(top), izquierda (left), derecha (right), abajo (bottom)
@@ -146,7 +199,7 @@ ggplot(df_distribucion_sin_totales) +
 
 #Diagrama de barras de df "df_distribucion_sin_totales" categorizado por periodo (año)
 ggplot(df_distribucion_sin_totales) +
-  geom_col(aes(x = Usuarios, y = Total, fill = periodo ), position = "dodge") +
+  geom_col(aes(x = grupos_usuarios, y = total, fill = periodo ), position = "dodge") +
   labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
   theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
         # arriba(top), izquierda (left), derecha (right), abajo (bottom)
@@ -155,33 +208,26 @@ ggplot(df_distribucion_sin_totales) +
 
 #Diagrama de dispersión de df "df_distribucion_sin_totales" categorizado por tipos de usuarios
 ggplot(df_distribucion_sin_totales) +
-  geom_point(aes(x = periodo, y = Total, color = Usuarios)) +
+  geom_point(aes(x = periodo, y = total, color = grupos_usuarios)) +
   labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
   theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8)) 
 
 #Diagrama de dispersión de df "df_distribucion_sin_totales" categorizado por tipos de usuarios de manera individual en vertical
 ggplot(df_distribucion_sin_totales) +
-  geom_point(aes(x = periodo, y = Total, color = Usuarios)) +
-  facet_grid( Usuarios ~ .) +
+  geom_point(aes(x = periodo, y = total, color = grupos_usuarios)) +
+  facet_grid( grupos_usuarios ~ .) +
   labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
   theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
         legend.position = "bottom"  ) 
 
 #Diagrama de dispersión de df "df_distribucion_sin_totales" categorizado por tipos de usuarios de manera individual en diferentes diagramas
 ggplot(df_distribucion_sin_totales) +
-  geom_point(aes(x = periodo, y = Total, color = Usuarios)) +
-  facet_wrap( Usuarios ~ ., nrow = 2) +
+  geom_point(aes(x = periodo, y = total, color = grupos_usuarios)) +
+  facet_wrap( grupos_usuarios ~ ., nrow = 2) +
   labs(subtitle = "Distribución de agua registrada, usuario y periodo.csv") +
   theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1, size = 8),
         legend.position = "bottom"  )
 #FIN#____________VISUALIZACIÓN DE LOS DFs____________#FIN#
-
-view(df_captacion)
-view(df_distribucion)
-view(df_volumen)
-view(df_distribucion_categorizada)
-view(df_volumen_categorizada)
-view(df_residuales)
 
 #######################################################################################################
 #######################################################################################################
